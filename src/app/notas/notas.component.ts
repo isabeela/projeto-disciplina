fcf271e2c19997266a1b37e9a8db324599063d28
isabeela <isabeelabg@gmail.com>
2021-09-15 20:48:03 -0300

    1 file modified, 1 added

--- src/app/notas/notas.component.ts
+++ src/app/notas/notas.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ManutencaoService } from '../manutencao.service';
 
 @Component({
   selector: 'app-notas',
@@ -7,23 +6,10 @@ import { ManutencaoService } from '../manutencao.service';
   styleUrls: ['./notas.component.css']
 })
 export class NotasComponent implements OnInit {
-  p1: String;
-  p2: String;
-  id: number
 
-  constructor(public manutencaoService: ManutencaoService) {}
+  constructor() { }
 
-  ngOnInit() {}
-
-  handleClick() {
-   
-
-    this.manutencaoService.updateGrades(
-      this.p1,
-      this.p2,
-      this.id
-    )
-    this.p1 = '';
-    this.p2 = '';
+  ngOnInit() {
   }
+
 }

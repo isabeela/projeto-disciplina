1458d58d096179184ebcfa3554a4f9544b4c3b74
isabeela <isabeelabg@gmail.com>
2021-09-15 20:11:13 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DisciplinaComponent } from './disciplina/disciplina.component';
 import { NotasComponent } from './notas/notas.component';
+
 import { ManutencaoService } from './manutencao.service';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'disciplina', component: DisciplinaComponent },
+      { path: 'notas', component: NotasComponent }
+    ])
+  ],
+
   declarations: [
     AppComponent,
     HelloComponent,
